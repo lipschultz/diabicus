bce5e502ac0c3103361122c78653b95c9099f0b0
Michael Lipschultz <michael.lipschultz@gmail.com>
2016-07-16 21:19:24 -0400

    Make explicit Ans*Ans multiplication

--- src/calculator.py
+++ src/calculator.py
@@ -25,6 +25,7 @@ def make_operations_explicit(expr):
     expr = re.sub(r'([0-9).]?)\(', lambda m: '(' if len(m.group(1)) == 0 else m.group(1)+'*(', expr)
     expr = re.sub(r'\)([0-9.]?)', lambda m: ')' if len(m.group(1)) == 0 else ')*'+m.group(1), expr)
     expr = re.sub('([0-9).]?)'+FUNCTION_PREFIX, lambda m: '' if len(m.group(1)) == 0 else m.group(1)+'*', expr)
+    expr = expr.replace('AnsAns', 'Ans*Ans')
     return expr
 
 def eval_expr(evaluator, expr):
